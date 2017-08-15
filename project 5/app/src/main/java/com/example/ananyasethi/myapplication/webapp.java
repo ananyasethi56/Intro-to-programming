35a77fa11baded8a1dbd13caf3bb7746c71c58ea
ananyasethi56 <31038768+ananyasethi56@users.noreply.github.com>
2017-08-15 19:26:09 +0530

    Add files via upload

---
+++ project 5/app/src/main/java/com/example/ananyasethi/myapplication/webapp.java
@@ -0,0 +1,13 @@
+package com.example.ananyasethi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class webapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webapp);
+    }
+}
